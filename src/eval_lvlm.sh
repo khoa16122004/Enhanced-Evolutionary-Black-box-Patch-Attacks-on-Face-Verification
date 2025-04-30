7446ec569e3d8692732363dabcd05c2c68ea9b1e
khoa16122004 <khoatr.work@gmail.com>
2025-04-30 16:26:11 +0700

    Update eval_lvlm.sh

--- src/eval_lvlm.sh
+++ src/eval_lvlm.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks-per-node=1
 
 
-python evaluate_lvlm.py --pretrained llava-onevision-qwen2-7b-ov --model_name llava --dataset lfw
-python evaluate_lvlm.py --pretrained llava-next-interleave-7b --model_name llava --dataset lfw
+python evaluate_lvlm.py --pretrained llava-onevision-qwen2-7b-ov --model_name llava_qwen --dataset lfw
+python evaluate_lvlm.py --pretrained llava-next-interleave-7b --model_name llava_qwen --dataset lfw
 python evaluate_lvlm.py --pretrained Mantis-8B-clip-llama3 --model_name mantis --dataset lfw
 python evaluate_lvlm.py --pretrained Mantis-8B-siglip-llama3 --model_name mantis --dataset lfw
 python evaluate_lvlm.py --pretrained deepseek-vl-7b-chat --model_name deepseek --dataset lfw

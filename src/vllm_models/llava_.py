b736abda02fc4dafb5e6f8b3eb366d4540f41719
khoa16122004 <khoatr.work@gmail.com>
2025-04-30 16:13:23 +0700

    Update llava_.py

--- src/vllm_models/llava_.py
+++ src/vllm_models/llava_.py
@@ -37,8 +37,6 @@ class LLava:
         image_tensors = [_image.to(dtype=torch.float16, device=self.device) for _image in image_tensors]
         image_sizes = [image.size for image in img_files]
         
-        print("image_size: ", image_sizes)
-        print("image_tensors: ", image_tensors) 
         with torch.inference_mode():
             cont = self.model.generate(
             input_ids,

f87ef6764bc95e2e4299a0f0c599e8652ba520b3
khoa16122004 <khoatr.work@gmail.com>
2025-04-30 16:09:14 +0700

    Update llava_.py

--- src/vllm_models/llava_.py
+++ src/vllm_models/llava_.py
@@ -36,8 +36,7 @@ class LLava:
         image_tensors = process_images(img_files, self.image_processor, self.model.config)
         image_tensors = [_image.to(dtype=torch.float16, device=self.device) for _image in image_tensors]
         image_sizes = [image.size for image in img_files]
-        # attention_mask = torch.ones_like(input_ids)
-           
+                    
         with torch.inference_mode():
             cont = self.model.generate(
             input_ids,
@@ -46,8 +45,6 @@ class LLava:
             do_sample=False,
             temperature=0,
             max_new_tokens=4096,
-            pad_token_id=self.tokenizer.pad_token_id,
-            # attention_mask=attention_mask,
         )
 
         text_outputs = self.tokenizer.batch_decode(cont, skip_special_tokens=True)

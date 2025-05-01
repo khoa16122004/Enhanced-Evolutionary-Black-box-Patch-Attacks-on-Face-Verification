72b05d6bb7db7e72997b7ec536dab1d6d43aa4c5
khoa16122004 <khoatr.work@gmail.com>
2025-05-01 10:36:57 +0700

    Update gemini_extract.py

--- src/gemini_extract.py
+++ src/gemini_extract.py
@@ -144,3 +144,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--response_path", type=str, default="")
     args = parser.parse_args()
+    main(args)

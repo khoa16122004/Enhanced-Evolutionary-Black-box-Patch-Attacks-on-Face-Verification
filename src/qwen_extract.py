49253a71a4dc007703ffec094c1fd4758cae2307
khoa16122004 <khoatr.work@gmail.com>
2025-05-01 17:06:15 +0700

    Update qwen_extract.py

--- src/qwen_extract.py
+++ src/qwen_extract.py
@@ -18,7 +18,7 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map="auto",
     trust_remote_code=True
 ).eval()
-model.generation_config = GenerationConfig.from_pretrained('./', pad_token_id=tokenizer.pad_token_id)
+model.generation_config = GenerationConfig.from_pretrained('Qwen/Qwen-7B-Chat', pad_token_id=tokenizer.pad_token_id)
 
 all_raw_text = ["我想听你说爱我。", "今天我想吃点啥，甜甜的，推荐下", "我马上迟到了，怎么做才能不迟到"]
 batch_raw_text = []

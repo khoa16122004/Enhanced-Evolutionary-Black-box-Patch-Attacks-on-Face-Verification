c8bbcc93151066f37455016e5e279a4c98f4f20a
khoa16122004 <khoatr.work@gmail.com>
2025-05-05 13:34:10 +0700

    Update flow_eval.py

--- src/flow_eval.py
+++ src/flow_eval.py
@@ -41,7 +41,7 @@ class Agent:
             combined_descriptions += f"Description {i+1}:\n{output}\n\n"
 
         summary_prompt = (
-            "You are given multiple reasoning descriptions based on two facial images. "
+            "You are given several reasoning descriptions comparing two facial images, along with the images themselves."
             "Some descriptions may conflict. Pay close attention to the details and disagreement. "
             "Then conclude: are these **exactly the same person**, **likely the same**, or **clearly different**?\n\n"
             f"{combined_descriptions}"
@@ -86,7 +86,7 @@ def main(args):
                 outputs.append((j, response))
                 # break
 
-            output_path = os.path.join(prompt_dir, f"{args.label}_{args.pretrained}_{args.dataset}_{args.model_name}.txt")
+            output_path = os.path.join(prompt_dir, f"{args.label}_{args.lvlm_pretrained}_{args.dataset}_{args.lvlm_model_name}.txt")
             with open(output_path, "w") as f:
                 for o in outputs:
                     f.write(f"{o[0]}\t{o[1]}\n")

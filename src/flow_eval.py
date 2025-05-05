3d7e345bab255efd03c3a628391080bc1fe94cd2
khoa16122004 <khoatr.work@gmail.com>
2025-05-05 10:20:07 +0700

    Update flow_eval.py

--- src/flow_eval.py
+++ src/flow_eval.py
@@ -5,10 +5,14 @@ from PIL import Image
 from tqdm import tqdm
 
 class Agent:
-    def __init__(self, lvlm, eval_lvlm, steps):
+    def __init__(self, lvlm, lvlm_image_token,
+                 eval_lvlm, eval_lvlm_image_token,
+                 steps):
         self.lvlm = lvlm
         self.steps = steps
         self.eval_lvlm = eval_lvlm
+        self.lvlm_image_token = lvlm_image_token
+        self.eval_lvlm_image_token = eval_lvlm_image_token
                 
     def eval(self, img_files, temperature=0.5):
         prompt_base = (
@@ -16,7 +20,9 @@ class Agent:
             "Then decide whether they depict the same person."
         )
         for i in range(self.steps):
-            output = self.lvlm.inference(prompt_base, img_files, 
+            
+            output = self.lvlm.inference(prompt_base + self.lvlm_image_token * 2, 
+                                         img_files, 
                                          temperature=temperature, reload=False)[0].replace("\n", "")
             
             
@@ -24,9 +30,10 @@ class Agent:
                 "Given the description below about two facial images, "
                 "decide whether it is a correct and sufficient explanation to conclude identity. "
                 "If yes, return 'True', otherwise return 'False'.\n\n"
-                f"Description: \n{output}"
+                f"Description: \n{output}\n Images: "
             )
-            eval_output = self.eval_lvlm.inference(final_prompt, img_files, 
+            eval_output = self.eval_lvlm.inference(final_prompt + self.eval_lvlm_image_token * 2, 
+                                                   img_files, 
                                                    temperature=temperature, reload=False)[0].replace("\n", "")
             print(f"Step {i}: Large VLM output: {output}, Eval output: {eval_output}\n")
 
@@ -36,15 +43,18 @@ class Agent:
         return None
 
 def main(args):
-    lvlm = init_lvlm_model(args.lvlm_pretrained, args.lvlm_model_name)
-    eval_lvlm = init_lvlm_model(args.eval_lvlm_pretrained, args.eval_lvlm_model_name)
+    lvlm_model, lvlm_image_token, lvlm_special_token = init_lvlm_model(args.lvlm_pretrained, args.lvlm_model_name)
+    
+    eval_lvlm, eval_lvlm_image_token, eval_lvlm_special_token = init_lvlm_model(args.eval_lvlm_pretrained, args.eval_lvlm_model_name)
     with torch.no_grad():
         img1 = Image.open(args.img1_path).convert("RGB")
         img2 = Image.open(args.img2_path).convert("RGB")
 
 
         
-        agent = Agent(lvlm, eval_lvlm, args.steps)
+        agent = Agent(lvlm_model, lvlm_image_token,
+                      eval_lvlm, eval_lvlm_image_token,
+                      args.steps)
         
         # lvlm.reload()
         # eval_lvlm.reload()

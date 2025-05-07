51881daca4fdbf341421190b8cf8982e21f1b173
khoa16122004 <khoatr.work@gmail.com>
2025-05-07 17:58:46 +0700

    Update deepseek_.py

--- src/vllm_models/deepseek_.py
+++ src/vllm_models/deepseek_.py
@@ -44,7 +44,7 @@ class DeepSeek:
             pad_token_id=self.tokenizer.eos_token_id,
             bos_token_id=self.tokenizer.bos_token_id,
             eos_token_id=self.tokenizer.eos_token_id,
-            max_new_tokens=512,
+            max_new_tokens=4096,
             do_sample=do_sample,
             use_cache=True,
             num_return_sequences=num_return_sequences,

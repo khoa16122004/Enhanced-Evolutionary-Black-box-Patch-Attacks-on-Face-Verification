0848d2d49ff43f795f7105c3211802e5707c1dcb
khoa16122004 <khoatr.work@gmail.com>
2025-05-08 17:01:44 +0700

    Update eval_lvlm_question.sh

--- src/eval_lvlm_question.sh
+++ src/eval_lvlm_question.sh
@@ -4,5 +4,4 @@
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 
-python facial_verification_question_eval_.py --lvlm_pretrained llava-onevision-qwen2-7b-ov --lvlm_model_name llava_qwen
 python facial_verification_question_eval_.py --lvlm_pretrained llava-next-interleave-7b --lvlm_model_name llava_qwen

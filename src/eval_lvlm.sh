dfe42a3067b4f826d1ebf0431c3bf21263ee1765
khoa16122004 <khoatr.work@gmail.com>
2025-05-10 11:14:29 +0700

    Update eval_lvlm.sh

--- src/eval_lvlm.sh
+++ src/eval_lvlm.sh
@@ -3,6 +3,5 @@
 #SBATCH -N 1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-python facial_verification_lvlm.py --return_result 1 --pretrained llava-onevision-qwen2-7b-ov --model_name llava_qwen --dataset lfw
-python facial_verification_lvlm.py --return_result 1 --pretrained llava-next-interleave-7b --model_name llava_qwen --dataset lfw
-python facial_verification_lvlm.py --return_result 1 --pretrained Mantis-8B-clip-llama3 --model_name mantis --dataset lfw
+python facial_verification_lvlm.py --return_result 0 --pretrained llava-onevision-qwen2-7b-ov --model_name llava_qwen --dataset lfw
+python facial_verification_lvlm.py --return_result 0 --pretrained llava-next-interleave-7b --model_name llava_qwen --dataset lfw

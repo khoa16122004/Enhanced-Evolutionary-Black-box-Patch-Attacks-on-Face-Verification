399ceca98b095395cd9073a687cbebd49662c763
khoa16122004 <khoatr.work@gmail.com>
2025-05-16 11:55:17 +0700

    Update facial_verification_question_eval_.py

--- src/facial_verification_question_eval_.py
+++ src/facial_verification_question_eval_.py
@@ -38,13 +38,13 @@ class AgentWithDetailedQuestions:
         conclusion_prompt = (
             "Given the responses describing facial features in two images, imagine each response casts a vote on whether the images show the same person or different people."
             "Give more weight to responses that highlight differences in biometric features"
-            "Based on the overall 'vote', return only one word: same or different."
+            "Based on the overall 'vote'"
             f"{all_responses}\n"
         )
         
         final_decision = self.lvlm.inference(
             conclusion_prompt + self.lvlm_image_token * 2,
-            None, num_return_sequences=1,
+            img_files, num_return_sequences=1,
             do_sample=True, temperature=0.8, reload=False
         )
         print(len(final_decision))

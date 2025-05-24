af3b8c4128aea6def470653409e36250bbc0e210
khoa16122004 <khoatr.work@gmail.com>
2025-05-24 12:28:29 +0700

    Update eval_lvlm_question.sh

--- src/eval_lvlm_question.sh
+++ src/eval_lvlm_question.sh
@@ -3,4 +3,4 @@
 #SBATCH -N 1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-python facial_verification_question_eval_.py --lvlm_pretrained llava-next-interleave-7b --lvlm_model_name llava_qwen --dataset lfw --num_samples 3 --extract_llm Llama-7b
+python facial_verification_question_eval_.py --lvlm_pretrained llava-next-interleave-7b --lvlm_model_name llava_qwen --dataset lfw_original --num_samples 3 --extract_llm Llama-7b

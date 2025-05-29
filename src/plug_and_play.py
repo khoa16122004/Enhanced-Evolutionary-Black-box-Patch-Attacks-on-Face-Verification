75d62e3f3257a1f806feabf94539f097e1f07714
khoa16122004 <khoatr.work@gmail.com>
2025-05-29 15:00:27 +0700

    Update plug_and_play.py

--- src/plug_and_play.py
+++ src/plug_and_play.py
@@ -8,10 +8,9 @@ llm = LlamaService(model_name="Llama-7b")
 lvlm_model, lvlm_image_token, lvlm_special_token = init_lvlm_model("llava-next-interleave-7b", "llava_qwen")
 max_rounds = 10
 initial_question = "Describe the person in the image."
-
 img_files = [
-    Image.open("../lfw_dataset/lfw_original/Adel_Al-Jubeir/Adel_Al-Jubeir_0001.jpg").convert("RGB"),
-    Image.open("../lfw_dataset/lfw_original/Ziwang_Xu/Ziwang_Xu_0001.jpg").convert("RGB")
+    Image.open("../lfw_dataset/lfw_original/Zarai_Toledo/Zarai_Toledo_0001.jpg").convert("RGB"),
+    Image.open("../lfw_dataset/lfw_original/Zarai_Toledo/Zarai_Toledo_0002.jpg").convert("RGB")
 ]
 
 llm_system_prompt = """

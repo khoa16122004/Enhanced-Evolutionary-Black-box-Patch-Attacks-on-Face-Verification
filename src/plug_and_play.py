9e31d0b7c91d2c5b544f4c19a31f83fa58d86d5f
khoa16122004 <khoatr.work@gmail.com>
2025-05-30 16:36:56 +0700

    Update plug_and_play.py

--- src/plug_and_play.py
+++ src/plug_and_play.py
@@ -41,7 +41,7 @@ llm_prompt_template = "History:\n{history}"
 
 # ==== Gameplay ====
 history = []
-system_vlm_prompt = "Just answer the question in the short ways, don't provide any information about the images.\n"
+system_vlm_prompt = "Just answer yes or no.\n"
 question = initial_question
 
 for round_idx in range(max_rounds):
